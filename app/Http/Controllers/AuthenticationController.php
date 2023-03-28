f9f77272604b017382a2608bc369af7e807fa240
Yohan Prasetyo <yohanprasetyonugrohoputra@gmail.com>
2023-03-28 21:25:49 +0700

    feat: authentication

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthenticationController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required',
+            'device_name' => 'required|string',
+        ]);
+
+        $user = User::whereEmail($request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => 'Alamat email atau password salah',
+            ]);
+        }
+
+        return [
+            'access_token' => $user->createToken($request->device_name)->plainTextToken,
+            'user' => $user,
+        ];
+    }
+
+    public function logout(Request $request)
+    {
+        return $request->user()->currentAccessToken()->delete();
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|confirmed',
+        ]);
+
+        return User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+    }
+
+    public function profile(Request $request)
+    {
+        return $request->user();
+    }
+}
