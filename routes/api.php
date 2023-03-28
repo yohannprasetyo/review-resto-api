86a1f7337c1ca41b2a44b23d80e07d6ede54d576
Yohan Prasetyo <yohanprasetyonugrohoputra@gmail.com>
2023-03-28 22:41:44 +0700

    fix: typo on route middleware

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,12 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/login', [AuthenticationController::class, 'login'])->name('auth.login');
 Route::post('/auth/register', [AuthenticationController::class, 'register'])->name('auth.register');
 
-Route::middleware('auth:santum')->group(function () {
-    Route::get('/auth/profile', [AuthenticationController::class, 'profile'])->name('auth.profile');
-    Route::get('/auth/logout', [AuthenticationController::class, 'logout'])->name('auth.logout');
-
+Route::middleware('auth:sanctum')->group(function () {
+  Route::get('/auth/profile', [AuthenticationController::class, 'profile'])->name('auth.profile');
+  Route::get('/auth/logout', [AuthenticationController::class, 'logout'])->name('auth.logout');
 });
